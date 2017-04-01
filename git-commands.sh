9401bc57ceb4aa610fb8b1a305a8c47e3b2fce51
Christopher Welborn <cj@welbornprod.com>
2017-03-31 19:52:28 -0500

    git-commands: Better support for duplicate commands.

--- git-commands.sh
+++ git-commands.sh
@@ -48,15 +48,71 @@ function fail_usage {
 }
 
 function format_subcommand_short {
-    # Colorize a subcommand and it's path.
-    local name=$1 cmdpath=$2
+    # Colorize a subcommand and it's path, with optional duplicate marker.
+    local name=$1 cmdpath=$2 dupecnt=$3
     { [[ -n "$name" ]] && [[ -n "$cmdpath" ]]; } || {
         echo_err "Missing name/cmdpath for format_subcommand_short!"
         return 1
     }
-    printf "%s: %s" \
-        "$(colr "$(printf "%-25s" "$name")" "blue")" \
-        "$(colr "$cmdpath" "green")"
+    local namewidth=25
+    if [[ -n "$dupecnt" ]]; then
+        namewidth=$((namewidth - ${#name} - ${#dupecnt} - 2))
+        printf "%s (%s%s: %s" \
+            "$(colr "$name" "blue")" \
+            "$(colr "$dupecnt" "red" "reset" "bright")" \
+            "$(printf "%-*s" "$namewidth" ")")" \
+            "$(colr "$cmdpath" "green")"
+    else
+        printf "%s: %s" \
+            "$(colr "$(printf "%-*s" "$namewidth" "$name")" "blue")" \
+            "$(colr "$cmdpath" "green")"
+    fi
+}
+
+function get_sub_commands {
+    # Lists git subcommands in PATH, with optional filtering by pattern.
+    # Arguments:
+    #   $1 : Filter pattern. Print only command names matching this pattern.
+    local pathdir cmdpath cmdexename cmdname cmdtype dupecnt dupename
+    local user_pattern=$1
+    # `subcmds` is local, but `gitsubcmds` is global.
+    declare -a subcmds
+    while IFS=$'\n' read -r pathdir; do
+        subcmds=("$pathdir"/git-*)
+        for cmdpath in "${subcmds[@]}"; do
+            if ((do_local)) && [[ "$cmdpath" != /home/* ]]; then
+                debug "Skipping non-local command: $cmdpath"
+                continue
+            fi
+            # Ignore commands that don't match the pattern, when given.
+            if [[ -n "$user_pattern" ]] && [[ ! "$cmdpath" =~ $user_pattern ]]; then
+                debug "Skipping non-matching command: $cmdpath"
+                continue
+            fi
+            cmdexename="${cmdpath##*/}"
+            cmdname="${cmdexename##git-}"
+
+            # Ignore duplicate command names, and go by $PATH.
+            if [[ -z "${gitsubcmds[$cmdname]}" ]]; then
+                gitsubcmds[$cmdname]="$cmdpath"
+                printf "%s\n" "$(format_subcommand_short "$cmdname" "$cmdpath")"
+            else
+                # Duplicate command.
+                ((do_duplicates)) && {
+                    dupecnt=2
+                    dupename="${cmdname} ($dupecnt)"
+                    while [[ -n "${gitsubcmds[$dupename]}" ]]; do
+                        ((dupecnt++))
+                        dupename="${cmdname} (${dupecnt})"
+                    done
+                    gitsubcmds[$dupename]="$cmdpath"
+                    printf "%s\n" "$(format_subcommand_short "$cmdname" "$cmdpath" "$dupecnt")"
+                }
+            fi
+        done
+    done < <(printf "%s" "$PATH" | tr ':' '\n')
+    ((${#gitsubcmds[@]})) || return 1
+    return 0
 }
 
 function print_usage {
@@ -65,24 +121,33 @@ function print_usage {
 
     echo "$appname v. $appversion
 
+    Lists git subcommands and their location.
+
     Usage:
         $appscript -h | -v
         $appscript [-D] [-l] [PATTERN]
 
     Options:
-        PATTERN       : Only show subcommands with a file path matching
-                        PATTERN (a text or regex pattern).
-        -D,--debug    : Print some debugging info while running.
-        -h,--help     : Show this message.
-        -l,--local    : Show local git subcommands only, no system commands.
-        -v,--version  : Show $appname version and exit.
+        PATTERN          : Only show subcommands with a file path matching
+                           PATTERN (a text or regex pattern).
+        -D,--debug       : Print some debugging info while running.
+        -d,--duplicates  : Show duplicate commands, that would never be
+                           callable as a git subcommand due to \$PATH issues.
+                           This only works when listing all commands.
+        -h,--help        : Show this message.
+        -l,--local       : Show local git subcommands only, not builtin.
+        -v,--version     : Show $appname version and exit.
     "
 }
 
 declare -a userargs
 do_local=0
+do_duplicates=0
 for arg; do
     case "$arg" in
+        "-d"|"--duplicates" )
+            do_duplicates=1
+            ;;
         "-D"|"--debug" )
             debug_mode=1
             ;;
@@ -109,26 +174,13 @@ user_pattern=""
 ((${#userargs[@]})) && user_pattern="${userargs[0]}"
 
 declare -A gitsubcmds
-while IFS=$'\n' read -r pathdir; do
-    subcmds=("$pathdir"/git-*)
-    for cmdpath in "${subcmds[@]}"; do
-        if ((do_local)) && [[ "$cmdpath" != /home/* ]]; then
-            debug "Skipping non-local command: $cmdpath"
-            continue
-        fi
-        # Ignore commands that don't match the pattern, when given.
-        if [[ -n "$user_pattern" ]] && [[ ! "$cmdpath" =~ $user_pattern ]]; then
-            debug "Skipping non-matching command: $cmdpath"
-            continue
-        fi
-        cmdexename="${cmdpath##*/}"
-        cmdname="${cmdexename##git-}"
-
-        # Ignore duplicate command names, and go by $PATH.
-        [[ -z "${gitsubcmds[$cmdname]}" ]] && gitsubcmds[$cmdname]="$cmdpath"
-        printf "%s\n" "$(format_subcommand_short "$cmdname" "$cmdpath")"
-    done
-done < <(printf "%s" "$PATH" | tr ':' '\n')
+get_sub_commands "$user_pattern" || {
+    failmsg="."
+    cmdtype=""
+    ((do_local)) && cmdtype="local "
+    [[ -n "$user_pattern" ]] && failmsg=" with pattern: $(colr "$user_pattern" "blue")"
+    fail "No ${cmdtype}subcommands found${failmsg}"
+}
 
 cmdtype="Subcommand"
 ((do_local)) && cmdtype="Local subcommand"
@@ -137,8 +189,3 @@ lbl="$(colr "${cmdtype}s found" "cyan")"
 printf "\n%s: %s\n" \
     "$lbl" \
     "$(colr "${#gitsubcmds[@]}" "blue" "reset" "bright")"
-
-let exitcode=0
-((${#gitsubcmds[@]} > 0)) || exitcode=1
-
-exit $exitcode
