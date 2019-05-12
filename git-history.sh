25667cc0addeb7dd7870c26aba1876056b382b08
Christopher Welborn <cj@welbornprod.com>
2019-05-12 15:07:43 -0500

    Bump version for latest changes.

--- git-history.sh
+++ git-history.sh
@@ -3,7 +3,7 @@
 # Shortcut to `git log --follow -p -- FILE`
 # -Christopher Welborn 07-11-2015
 appname="git-history"
-appversion="0.0.6"
+appversion="0.0.7"
 apppath="$(readlink -f "${BASH_SOURCE[0]}")"
 appscript="${apppath##*/}"
 
